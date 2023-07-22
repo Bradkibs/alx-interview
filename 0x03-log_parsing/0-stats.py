55cbe24efb36db819faa8aa1c795ac6b7a4ac600
Bradkibs <Bradleykibs@gmail.com>
2023-07-22 17:11:27 +0300

    fixing the printing implementation

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -14,7 +14,6 @@ import signal
 count = 0
 pattern = r'\b(?:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b - \[(.*?)\] ' \
           r'"GET \/projects\/260 HTTP\/1\.1" \d+ \d+'
-status = list()
 file_size = 0
 
 
@@ -29,24 +28,31 @@ signal.signal(signal.SIGINT, keyboard_interrupt_handler)
 
 def print_stats():
     """Prints all the statuses when needed"""
-    output = Counter(status)
     print(f'File size: {file_size}')
-    for stat, count in sorted(output.items()):
-        print(f"{stat}: {count}")
+    for stat, count in sorted(statuses.items()):
+        if count > 0:
+            print(f'{stat}: {count}')
+
+
+statuses = {
+        '200': 0,
+        '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
 
 
 for line in sys.stdin:
     match = re.search(pattern, line.strip())
     if match:
         try:
-            if count < 10:
-                count += 1
-                log = line.split(" ")
-                status.append(log[7])
-                file_size += int(log[8])
-            else:
+            count += 1
+            log = line.split()
+            if log[7] in statuses:
+                statuses[log[7]] += 1
+            file_size += int(log[8])
+            if count == 10:
                 print_stats()
                 count = 0
         except KeyboardInterrupt:
             print_stats()
             sys.exit(0)
+        except TypeError:
+            continue
