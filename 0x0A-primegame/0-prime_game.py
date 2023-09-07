f023b110de048dda22b1714717bb3ec19b4317b1
Bradkibs <Bradleykibs@gmail.com>
2023-09-07 20:00:16 +0300

    finally figured it

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,6 +4,21 @@ Define isWineer function, a solution to the Prime Game problem
 """
 
 
+def primes(n):
+    """Return list of prime numbers between 1 and n inclusive
+        Args:
+        n (int): upper boundary of range. lower boundary is always 1
+    """
+    prime = []
+    sieve = [True] * (n + 1)
+    for p in range(2, n + 1):
+        if (sieve[p]):
+            prime.append(p)
+            for i in range(p, n + 1, p):
+                sieve[i] = False
+    return prime
+
+
 def isWinner(x, nums):
     """
     Determines winner of Prime Game
@@ -13,20 +28,11 @@ def isWinner(x, nums):
     Return:
         Name of winner (Maria or Ben) or None if winner cannot be found
     """
-    def isPrime(n):
-        """Does a prime check on a given number"""
-        if n < 2:
-            return False
-        for i in range(2, int(n**0.5) + 1):
-            if n % i == 0:
-                return False
-        return True
-
     if x is None or nums is None or x == 0 or nums == []:
         return None
     Maria = Ben = 0
     for i in range(x):
-        prime = [i for i in range(2, item + 1) if isPrime(i)]
+        prime = primes(nums[i])
         if len(prime) % 2 == 0:
             Ben += 1
         else:
